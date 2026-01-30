5c710caa98614c5b61d6f6aeb565f4ac2f74df21
MertHasanAslan <merthasanaslan325@gmail.com>
2026-01-30 19:19:23 +0300

    companies.controller.ts done (will be changed)

--- kargogig-backend/src/main.ts
+++ kargogig-backend/src/main.ts
@@ -4,5 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix("api/v1");
 }
 bootstrap();
+
+
---
+++ kargogig-backend/src/modules/companies/companies.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@nestjs/common";
+import { CreateCompanyDto } from "./dto/create_company.dto"
+import { UpdateCompanyDto } from "./dto/update_company.dto"
+
+
+@Injectable
+export class CompaniesService{
+    
+}
